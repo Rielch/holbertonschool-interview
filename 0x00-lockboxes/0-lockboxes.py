ddb8d43e8bc1692fd1b0983bddcb493d274a3842
Rielch <2288@holbertonschool.com>
2021-12-09 21:43:38 +0000

    Fix part 0

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,7 +7,7 @@ each box may contain keys to the other boxes."""
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be opened"""
 
-    if type(boxes) is not list or len(boxes) == 0:
+    if type(boxes) is not list or len(boxes) == 0 or len(boxes[0]) == 0:
         return False
 
     def checkKeys(boxes, key, keys_list):
@@ -21,6 +21,4 @@ def canUnlockAll(boxes):
     keys_list = []
     checkKeys(boxes, 0, keys_list)
 
-    if len(keys_list) != len(boxes):
-        return False
-    return True
+    return len(boxes) == len(keys_list)
